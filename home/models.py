0b55a4d51cc905dd0922e7bf3e5a9a3dde486a1d
rujjal <rujjalsada25@gmail.com>
2021-07-25 21:45:52 +0545

    first commit

---
+++ home/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+# Create your models here.
+
+STATUS = (('active','active'),('inactive','inactive'))
+STOCK = (('In Stock','In Stock'),('Out of Stock','Out of Stock'))
+LABELS = (('new','new'),('hot','hot'),('sale','sale'),('','default'))
+
+class Category(models.Model):
+	title = models.CharField(max_length = 300)
+	description = models.TextField(blank = True)
+	slug = models.CharField(max_length = 500)
+	image = models.TextField(blank = True)
+
+	def __str__(self):
+		return (self.title)
+
+class SubCategory(models.Model):
+	title = models.CharField(max_length = 300)
+	category = models.ForeignKey(Category, on_delete = models.CASCADE)
+	description = models.TextField(blank = True)
+	slug = models.CharField(max_length = 500)
+	image = models.TextField(blank = True)
+
+	def __str__(self):
+		return (self.title)
+
+class Item(models.Model):
+	title = models.CharField(max_length = 300)
+	category = models.ForeignKey(Category, on_delete = models.CASCADE)
+	subcategory = models.ForeignKey(SubCategory, on_delete = models.CASCADE)
+	description = models.TextField(blank = True)
+	slug  = models.CharField(max_length = 500)
+	image = models.ImageField(upload_to = 'media')
+	price = models.IntegerField()
+	discounted_price = models.IntegerField()
+	status = models.CharField(max_length = 50, choices = STATUS, blank = True)
+	stock = models.CharField(max_length = 50, choices = STOCK, blank = True)
+	labels = models.CharField(max_length = 50, choices = LABELS, blank = True)
+
+	def __str__(self):
+		return (self.title)
+
+
